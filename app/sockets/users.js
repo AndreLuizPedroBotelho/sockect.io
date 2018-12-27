43000f63978d488fbc1951237dfb51f262cd25b3
André Luiz Pedro Botelho <andre-dede96@hotmail.com>
2018-12-27 13:47:04 -0200

    Commit 3

--- app/sockets/users.js
+++ app/sockets/users.js
@@ -1,5 +1,13 @@
 module.exports = function(io){
     io.sockets.on('connection',function(client){
         client.emit('hello',{title:'Bem vindo',msg:'Você está Conectado!'});
+    
+        client.on('user_add',function(data){
+            //broadcast mostra para os Outros usuarios
+            client.broadcast.emit('user_add_response',{
+                title:'Novo Professor',
+                msg: 'O professor ' + data.nome + ' foi contratado!'
+            })
+        })
     });
 }
--- public/javascripts/sockets.js
+++ public/javascripts/sockets.js
@@ -10,9 +10,43 @@ var notify = function (data, type){
         icon:'/images/' + type +'.png'
     });
 }
+
+
+
 var socket = io('http://localhost:3000');
 
 socket.on('hello', function(msg){
-    console.log(msg)
-    notify(msg, 'user')
+    //console.log(msg);
+    ///notify(msg, 'user')
 })
+
+socket.on('user_add_response', function(msg){
+    notify(msg, 'user_add');
+    $('main').load('/users #content_container');
+
+})
+
+$('#user_add').submit(function(e) {
+    var formDataArray = $(this).serializeArray();
+    var data = {
+      nome: null
+    }
+  
+    for (var i = 0; i < formDataArray.length; i++) {
+      if (formDataArray[i].name == 'nome') {
+        data.nome = formDataArray[i].value;
+      }
+    }
+  
+    $.ajax({
+      type: "POST",
+      url: $(this).attr('action'),
+      data: $(this).serialize(),
+      success: function() {
+        socket.emit('user_add', data);
+        window.location = '/users';
+      }
+    });
+    e.preventDefault();
+  });
+  
