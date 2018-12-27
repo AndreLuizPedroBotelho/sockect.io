2591eac237b21e481217ca17fe65c608cfe0632e
André Luiz Pedro Botelho <andre-dede96@hotmail.com>
2018-12-27 21:44:24 -0200

    Commit 3

--- app/sockets/users.js
+++ app/sockets/users.js
@@ -9,5 +9,22 @@ module.exports = function(io){
                 msg: 'O professor ' + data.nome + ' foi contratado!'
             })
         })
+
+        client.on('user_remove',function(data){
+            //broadcast mostra para os Outros usuarios
+            client.broadcast.emit('user_remove_response',{
+                title: 'Professor Removido',
+                msg: 'O professor ' + data.nome + ' foi demitido!'
+            })
+        })
+
+        client.on('user_edit',function(data){
+            console.log(data)
+            //broadcast mostra para os Outros usuarios
+            client.broadcast.emit('user_edit_response',{
+                title: 'Professor Editado',
+                msg: 'O professor ' + data.nome + ' foi editado!'
+            })
+        })
     });
 }
--- public/javascripts/sockets.js
+++ public/javascripts/sockets.js
@@ -11,8 +11,6 @@ var notify = function (data, type){
     });
 }
 
-
-
 var socket = io('http://localhost:3000');
 
 socket.on('hello', function(msg){
@@ -25,6 +23,53 @@ socket.on('user_add_response', function(msg){
     $('main').load('/users #content_container');
 
 })
+
+socket.on('user_edit_response', function(msg){
+    console.log('asas');
+    notify(msg, 'user_edit');
+    $('main').load('/users #content_container');
+
+})
+
+socket.on('user_remove_response', function(msg){
+    notify(msg, 'user_remove');
+    $('main').load('/users #content_container');
+
+})
+
+$('#user_remove').click(function(e){
+    var nome = $(this).data('nome');
+    $.get($(this).attr('href'),function(){
+        socket.emit('user_remove', {nome:nome});
+        window.location = '/users';
+    })
+    e.preventDefault();
+})
+
+$('#user_edit').submit(function(e) {
+    var formDataArray = $(this).serializeArray();
+    
+    var data = {
+      nome: null
+    }
+  
+    for (var i = 0; i < formDataArray.length; i++) {
+      if (formDataArray[i].name == 'nome') {
+        data.nome = formDataArray[i].value;
+      }
+    }
+  
+    $.ajax({
+      type: "POST",
+      url: $(this).attr('action'),
+      data: $(this).serialize(),
+      success: function() {
+        socket.emit('user_edit', data);
+        window.location = '/users';
+      }
+    });
+    e.preventDefault();
+  });
 
 $('#user_add').submit(function(e) {
     var formDataArray = $(this).serializeArray();
